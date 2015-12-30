4fac68d3c5d0f45f11172b37d4a637acfa69c8fc
Dione <dionemorales@outlook.com>
2015-12-30 20:47:35 +1100

    Added shadow effect

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer; // 12 hour time layer
+static TextLayer *time_shadow_layer;
 static TextLayer *battery_layer; //Battery display 
 static TextLayer *date_layer; //Current date display
 static TextLayer *inner_rect;
@@ -31,14 +32,16 @@ static void update_time(){
   
   // Write the current hours and minutes into a buffer
   static char s_buffer[8];
+  static char shadow_buffer[8];
   static char t_buffer[8];
   static char date_buffer[16];
+  strftime(shadow_buffer, sizeof(shadow_buffer), "%I:%M", tick_time);
   strftime(s_buffer, sizeof(s_buffer), "%I:%M", tick_time);
-  strftime(t_buffer, sizeof(s_buffer), "%H:%M", tick_time);
   strftime(date_buffer, sizeof(date_buffer), "%a %d %b", tick_time);
   
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
+  text_layer_set_text(time_shadow_layer, shadow_buffer);
   text_layer_set_text(date_layer, date_buffer);
 }
 
@@ -55,7 +58,7 @@ static void main_window_load(Window *window) {
   
   // Background
   background_second_half = text_layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
-  inner_rect = text_layer_create(GRect(12, 15, bounds.size.w - 24, bounds.size.h - 30));
+  inner_rect = text_layer_create(GRect(12, 16, bounds.size.w - 24, bounds.size.h - 32));
   text_layer_set_background_color(background_second_half, GColorDarkGray);
   text_layer_set_background_color(inner_rect, GColorLightGray);
   
@@ -71,10 +74,18 @@ static void main_window_load(Window *window) {
   // Time Displays
   s_time_layer = text_layer_create(GRect(0, bounds.size.h/2 - 30, bounds.size.w, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorElectricBlue );
+  text_layer_set_text_color(s_time_layer, GColorCeleste );
   text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_LECO_38_BOLD_NUMBERS));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  
+  //Time display shadow
+  time_shadow_layer = text_layer_create(GRect(0, bounds.size.h/2 - 27, bounds.size.w + 10, 45));
+  text_layer_set_background_color(time_shadow_layer, GColorClear);
+  text_layer_set_text_color(time_shadow_layer, GColorDarkGray );
+  text_layer_set_text(time_shadow_layer, "00:00");
+  text_layer_set_font(time_shadow_layer, fonts_get_system_font(FONT_KEY_LECO_38_BOLD_NUMBERS));
+  text_layer_set_text_alignment(time_shadow_layer, GTextAlignmentCenter);
   
   // Date
   date_layer = text_layer_create(GRect(0, bounds.size.h - 17, bounds.size.w, 50));
@@ -86,6 +97,7 @@ static void main_window_load(Window *window) {
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(background_second_half));
   layer_add_child(window_layer, text_layer_get_layer(inner_rect));
+  layer_add_child(window_layer, text_layer_get_layer(time_shadow_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   layer_add_child(window_layer, text_layer_get_layer(battery_layer));
   layer_add_child(window_layer, text_layer_get_layer(date_layer));
@@ -99,6 +111,7 @@ static void main_window_load(Window *window) {
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(time_shadow_layer);
   text_layer_destroy(battery_layer);
   text_layer_destroy(background_second_half);
   text_layer_destroy(date_layer);
